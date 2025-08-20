cf517685faf2c3ed9df9f8c68527ba073332516d
Priyanshu15092001 <priyanshughosh2001.pg@gmail.com>
2025-08-20 15:16:31 +0530

    Fixed cors error

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 connectDB();
  
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE,PATCH" }));
 app.use(express.json());
 
 // Routes
