d1f41f4936fea81c0e23ec7de7c448a48a6ef2a2
Priyanshu15092001 <priyanshughosh2001.pg@gmail.com>
2025-08-20 15:10:49 +0530

    Adding route for get all orders

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -58,3 +58,20 @@ exports.createOrder = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+exports.getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find()
+      .populate("user", "name email")        // populate user details
+      .populate("items.product", "name price stock"); // populate product details
+
+    if (!orders || orders.length === 0) {
+      return res.status(404).json({ message: "No orders found" });
+    }
+
+    res.status(200).json(orders);
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createOrder } = require("../controllers/orderControllers");
+const { createOrder,getAllOrders } = require("../controllers/orderControllers");
 const auth = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.post("/", auth, createOrder);
+router.get("/", auth, getAllOrders);
 
 module.exports = router;
